62d9d178e9ce26a20d132ea1c43f8ea4083ca575
Ngubane <Ppphum24@gmail.com>
2023-06-15 12:35:27 -0700

    c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - A program that prints the code with puts fuction
+ * return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return:(0);
+}
+
