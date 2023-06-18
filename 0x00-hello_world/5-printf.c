2d9f22bb15217427b8726d33aba8c5856e322874
Ngubane <Ppphum24@gmail.com>
2023-06-18 02:35:38 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf fuction
+ * Return:0(Program run with no errors)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
